a88674e1c9e52cc7a6aff635e43c5713f55ab745
Aryan Sethi <aryan.rdps@gmail.com>
2023-12-05 19:55:20 +0530

    ch4 init

---
+++ ch4/1_DOM_nodes.js
@@ -0,0 +1,7 @@
+var first_li = document.querySelector(".hot");
+
+console.log(first_li.firstChild);
+console.log(first_li.firstChild.nodeValue); //imp
+console.log(first_li.innerText);
+console.log(first_li.lastChild);
+console.log(first_li.firstElementChild);
